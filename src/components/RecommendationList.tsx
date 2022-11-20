d3c282620912d9e3d25445c75eda9132f67eacde
Ren Dela Cruz <ren_delacruz@outlook.com>
2022-11-19 23:49:02 -0500

    Changed mocked data

--- src/components/RecommendationList.tsx
+++ src/components/RecommendationList.tsx
@@ -1,36 +1,47 @@
 import { Stack } from '@mantine/core';
-import RecommendationView from "./DrinkRecommendationView";
 import info from '../database/data';
-import pcts from "../database/percentages";
+import pcts from '../database/percentages';
+import RecommendationView from './DrinkRecommendationView';
 
-export function RecommendationList(props : any) {
+export function RecommendationList(props: any) {
+  function compare(a: any, b: any) {
+    var bi = props.bitter ? 1 : 0;
+    var sw = props.sweet ? 1 : 0;
+    var so = props.sour ? 1 : 0;
+    var sp = props.spicy ? 1 : 0;
+    var fr = props.fruity ? 1 : 0;
+    var sm = props.smoky ? 1 : 0;
 
-  function compare( a : any, b : any) {
-    var bi = props.bitter ? 1 : 0
-    var sw = props.sweet ? 1 : 0
-    var so = props.sour ? 1 : 0
-    var sp = props.spicy ? 1 : 0
-    var fr = props.fruity ? 1 : 0
-    var sm = props.smoky ? 1 : 0
+    var count_a =
+      bi * a.percent.bitter +
+      sw * a.percent.sweet +
+      so * a.percent.sour +
+      sp * a.percent.spicy +
+      fr * a.percent.fruity +
+      sm * a.percent.smoky;
+    var count_b =
+      bi * b.percent.bitter +
+      sw * b.percent.sweet +
+      so * b.percent.sour +
+      sp * b.percent.spicy +
+      fr * b.percent.fruity +
+      sm * b.percent.smoky;
 
-    var count_a = bi*a.percent.bitter + sw*a.percent.sweet + so*a.percent.sour + sp*a.percent.spicy + fr*a.percent.fruity + sm*a.percent.smoky
-    var count_b = bi*b.percent.bitter + sw*b.percent.sweet + so*b.percent.sour + sp*b.percent.spicy + fr*b.percent.fruity + sm*b.percent.smoky
-    
-    return(count_b - count_a);
+    return count_b - count_a;
   }
 
   return (
     <div>
       <div>
-          <div>
-            {pcts.sort((compare)).map(({ pname, percent }) => 
-              <>
-                {info.map(({ category, drinks }) => (
-                      <Stack>
-                    {drinks.map((drink) => (
-                      <>
-                      {drink.name===pname.name ?(
-                        <RecommendationView 
+        <div>
+          {pcts.sort(compare).map(({ pname, percent }) => (
+            <>
+              {info.map(({ category, drinks }) => (
+                <Stack>
+                  {drinks.map((drink) => (
+                    <>
+                      {drink.name === pname.name ? (
+                        <RecommendationView
                           key={drink.name}
                           image={drink.image}
                           category={category}
@@ -38,14 +49,16 @@ export function RecommendationList(props : any) {
                           price={drink.price}
                           percentages={percent}
                         />
-                      ): ("")}
-                      </>
-                    ))}
-                      </Stack>
-                ))}
-              </>
-            )}
-          </div>
+                      ) : (
+                        ''
+                      )}
+                    </>
+                  ))}
+                </Stack>
+              ))}
+            </>
+          ))}
+        </div>
       </div>
     </div>
   );
--- src/database/percentages.ts
+++ src/database/percentages.ts
@@ -1,125 +1,124 @@
-
-
 interface Percentages {
   pname: {
-    name:string;
-  },
-  percent:{
+    name: string;
+  };
+  percent: {
     bitter: number;
     sweet: number;
     sour: number;
     spicy: number;
     fruity: number;
     smoky: number;
-  }
+  };
 }
 
 const pcts: Array<Percentages> = [
   {
     pname: {
-        name: 'Margarita',
+      name: 'Margarita',
     },
-    percent:{
+    percent: {
       bitter: 1,
       sweet: 50,
       sour: 3,
       spicy: 27,
       fruity: 14,
       smoky: 5,
-    }
+    },
   },
   {
     pname: {
-        name: 'Negroni',
+      name: 'Negroni',
     },
-    percent:{
+    percent: {
       bitter: 66,
       sweet: 1,
       sour: 8,
       spicy: 14,
       fruity: 5,
       smoky: 6,
-    }
+    },
   },
   {
     pname: {
-        name: 'Daiquiri',
+      name: 'Daiquiri',
     },
-    percent:{
+    percent: {
       bitter: 26,
       sweet: 46,
       sour: 15,
       spicy: 8,
       fruity: 1,
       smoky: 4,
-    }
+    },
   },
   {
     pname: {
-        name: 'Bousquet',
+      name: 'Bousquet',
     },
-    percent:{
+    percent: {
       bitter: 2,
       sweet: 12,
       sour: 3,
       spicy: 30,
       fruity: 39,
       smoky: 14,
-    }
+    },
   },
   {
     pname: {
-        name: 'Sandhill',
+      name: 'Sandhill',
     },
-    percent:{
+    percent: {
       bitter: 17,
       sweet: 22,
       sour: 18,
       spicy: 31,
       fruity: 5,
       smoky: 7,
-    }
+    },
   },
   {
     pname: {
-        name: 'Jägermeister',
+      name: 'Jägermeister',
     },
-    percent:{
+    percent: {
       bitter: 7,
       sweet: 1,
       sour: 62,
       spicy: 6,
       fruity: 5,
       smoky: 19,
-    }
+    },
   },
   {
     pname: {
-        name: 'Belgian Moon',
+      name: 'Belgian Moon',
     },
-    percent:{
+    percent: {
       bitter: 33,
       sweet: 5,
       sour: 18,
       spicy: 6,
       fruity: 28,
       smoky: 10,
-    }
+    },
   },
   {
     pname: {
-        name: 'Heineken',
+      name: 'Heineken',
     },
-    percent:{
+    percent: {
       bitter: 62,
       sweet: 6,
       sour: 18,
       spicy: 6,
       fruity: 3,
       smoky: 6,
-    }
+    },
   },
 ];
+
 export const allPcts = pcts.map(({ pname, percent }) => percent).flat(1);
 
 export default pcts;
